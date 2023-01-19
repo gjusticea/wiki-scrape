a066d4af121affd5d1942bba1fac144ba4358fc5
nikosbosse <nikosbosse@gmail.com>
2023-01-19 10:50:21 +0100

    simplify code for suggestion heuristics and add a new heuristic for columns of the largest table

--- scripts/List of oil spills.R
+++ scripts/List of oil spills.R
@@ -5,14 +5,12 @@ url <- "https://en.wikipedia.org/wiki/List_of_oil_spills"
 tables <- download_tables(url)
 suggested_tables <- suggest_tables_to_keep(tables)
 
-# in this case the heuristic with the most columns leads us astray. Maybe a
-# better approach would be to select the ones with the most rows and then
-# chose the number of columns that table has.
-ids_to_keep <- 2
+# in this case the heuristic with the most columns leads us astray.
+# selecting the table that has the highest number of columns works, though
+ids_to_keep <- suggested_tables[["cols max row"]]
 tables <- tables[ids_to_keep]
-
 table <- rbindlist(tables)
-names(table)
+
 
 ## To do: more cleaning to get it into the format we want.
 
