0a578558385123e226247148b414a56065e2bcdf
gjusticea <gjusticea@gmail.com>
2023-07-21 15:49:54 -0700

    Country key preparation
    Prep work for and addition of 'Country key clean.xlsx', the main country key for all of our lists (as of this submission)

--- ref/country key prep/Country key cleaning.R
+++ ref/country key prep/Country key cleaning.R
@@ -1,3 +1,4 @@
+source("utils/functions.R")
 
 # remove special characters
 rm_special = function(x){
@@ -25,52 +26,79 @@ ckey = fread("ref/country key prep/Country key wide raw.csv", header = TRUE) %>%
   filter(country_name != "") %>%
   mutate(country_name = rm_special(country_name),
          country_name = case_when(
-           country_name %in% c("Bahamas","Bahamas, The") ~ "The Bahamas",
+           country_name %in% c("Bahamas","Bahamas, The") ~ "Bahamas (the)",
            country_name == "Bolivia" ~ "Bolivia (Plurinational State of)",
-           grepl("Bosnia",country_name) ~ "Bosnia and Herzegovina",
+           country_name %in% c("Bosnia-Herzegovina","Bosnia & Herzegovina") ~ "Bosnia and Herzegovina",
            country_name %in% c("UK","Britain","United Kingdom") ~ "United Kingdom of Great Britain and Northern Ireland (the)",
            country_name == "British Virgin Islands" ~ "Virgin Islands (British)",
            country_name == "Brunei" ~ "Brunei Darussalam",
-           country_name == "Cape Verde" ~ "",
-           country_name == "Central African Republic" ~ "",
-           country_name == "Comoros" ~ "",
-           country_name == "Congo" ~ "",
+           country_name == "Cape Verde" ~ "Cabo Verde",
+           country_name == "Central African Republic" ~ "Central African Republic (the)",
+           country_name == "Comoros" ~ "Comoros (the)",
+           country_name == "Congo" ~ "Congo (the)",
            country_name %in% c("Congo, Dem. Rep.",
                                "Democratic Republic of Congo",
-                               "Democratic Republic of the Congo") ~ "",
+                               "Democratic Republic of the Congo") ~ "Congo (the Democratic Republic of the)",
            country_name %in% c("Congo, Rep.",
                                "Republic of Congo",
-                               "Republic of the Congo") ~ "",
-           country_name == "Czech Republic" ~ "",
-           country_name == "Dominican Republic" ~ "",
-           country_name == "Equitorial Guinea" ~ "",
-           country_name == "Egypt, Arab Rep" ~ "",
-           country_name %in% c("Gambia","Gambia, The") ~ "",
-           country_name %in% c("Iran","Iran, Islamic Rep.") ~ "",
-           country_name == "Ivory Coast" ~ "",
-           country_name %in% c("Korea, Dem. People's Rep.","North Korea") ~ "",
-           country_name %in% c("Korea, Rep.") ~ "",
-           country_name == "Kosovo" ~ "",
-           country_name == "Laos" ~ "",
-           country_name == "Macedonia" ~ "",
-           country_name == "Marshall Islands" ~ "",
-           country_name == "Mauritani" ~ "",
-           country_name %in% c("Micronesia","Micronesia, Fed. Sts.") ~ "",
-           country_name == "Moldova" ~ "",
-           country_name == "Netherlands" ~ "",
-           country_name == "Niger" ~ "",
-           country_name %in% c("Ottoman","Ottoman Empire") ~ "",
-           country_name == "Palestine" ~ "",
-           country_name == "Papua" ~ "",
-           country_name == "Philippines" ~ "",
-           country_name == "Rumania" ~ "",
-           country_name %in% c("Russia","Russian Federation") ~ "",
-           country_name == "Sao Tome & Principe" ~ "",
-           country_name == "Saudi" ~ ""
+                               "Republic of the Congo") ~ "Congo (the)",
+           country_name == "Czech Republic" ~ "Czechia",
+           country_name == "Dominican Republic" ~ "Dominican Republic (the)",
+           country_name == "Equitorial Guinea" ~ "Equatorial Guinea",
+           country_name %in% c("Gambia","Gambia, The") ~ "Gambia (the)",
+           country_name %in% c("Iran","Iran, Islamic Rep.") ~ "Iran (Islamic Republic of)",
+           country_name == "Ivory Coast" ~ "Cote d'Ivoire",
+           country_name %in% c("Korea, Dem. People's Rep.","North Korea") ~ "Korea (the Democratic People's Republic of)",
+           country_name %in% c("Korea, Rep.","South Korea") ~ "Korea (the Republic of)",
+           country_name == "Laos" ~ "Lao People's Democratic Republic (the)",
+           country_name == "Macedonia" ~ "North Macedonia",
+           country_name == "Marshall Islands" ~ "Marshall Islands (the)",
+           country_name %in% c("Micronesia","Micronesia, Fed. Sts.") ~ "Micronesia (Federated States of)",
+           country_name == "Moldova" ~ "Moldova (the Republic of)",
+           country_name == "Netherlands" ~ "Netherlands, Kingdom of the",
+           country_name == "Niger" ~ "Niger (the)",
+           country_name %in% c("Ottoman","Ottoman Empire") ~ "Ottoman Empire",
+           country_name == "Philippines" ~ "Philippines (the)",
+           country_name %in% c("Russia","Russian Federation") ~ "Russian Federation (the)",
+           country_name == "Sao Tome & Principe" ~ "Sao Tome and Principe",
+           country_name == "Saudi" ~ "Saudi Arabia",
+           country_name %in% c("St. Kitts & Nevis","St. Kitts and Nevis") ~ "Saint Kitts and Nevis",
+           country_name %in% c("St. Vincent & the Grenadines","St. Vincent and the Grenadines") ~ "Saint Vincent and the Grenadines",
+           country_name %in% c("Sint Maarten") ~ "Sint Maarten (Dutch part)",
+           country_name %in% c("St. Martin (French part)","St. Martin") ~ "Saint Martin (French part)",
+           country_name == "St. Lucia" ~ "Saint Lucia",
+           country_name == "Soviet Union" ~ "USSR",
+           country_name == "Sudan" ~ "Sudan (the)",
+           country_name == "Swaziland" ~ "Eswatini",
+           country_name == "Syria" ~ "Syrian Arab Republic (the)",
+           country_name == "Tanzania" ~ "Tanzania, the United Republic of",
+           country_name == "Trinidad & Tobago" ~ "Trinidad and Tobago",
+           country_name == "Turkey" ~ "Turkiye",
+           country_name %in% c("Turks and Caicos","Turks and Caicos Islands") ~ "Turks and Caicos Islands (the)",
+           country_name == "United Arab Emirates" ~ "United Arab Emirates (the)",
+           country_name %in% c("U.S.","US","USA","United States","United States of America") ~ "United States of America (the)",
+           country_name == "Vatican City/Holy See" ~ "Holy See (the)",
+           country_name == "Venezuela" ~ "Venezuela (Bolivarian Republic of)",
+           country_name == "Vietnam" ~ "Viet Nam",
+           country_name == "Virgin Islands" ~ "Virgin Islands (U.S.)",
+           country_name %in% c("West Bank and Gaza","West Bank and Gaza Strip","Palestine") ~ "Palestine, State of",
+           TRUE ~ country_name
          )) %>%
   merge(country_codes, by = "country_name", all.x = TRUE)
 
+ckey %>% filter(is.na(ccode)) %>%
+  count(country_name)
 
+# Add label type groups, if a single applies to a whole category
+ckey %<>%
+  mutate(ccode_type = case_when(
+    `Category ID` %in% paste0("G",c(16:63,67,7,81,9,3,84,14,15)) ~ "Polity",
+    `Category ID` %in% paste0("G",c(65,76)) ~ "Geography"
+  )) %>%
+  select(`Category ID`,Category,Event,`Event description`,
+         country_name, ccode, ccode_type) %>%
+  arrange(`Category ID`,Event)
 
+fwrite(ckey, file="ref/country key prep/Country key long raw.csv")
 
 
